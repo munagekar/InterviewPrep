038cb56ba7e5f31354526e58e43fd683020ac936
munagekar <avm.abhishek@gmail.com>
2018-11-24 22:43:55 +0900

    Add Kadane with indices

--- Algos/kadane.py
+++ Algos/kadane.py
@@ -13,6 +13,32 @@ def kadane(li):
     return t_max
 
 
+def kadane_with_indices(li):
+    cfirst = 0
+    clast = 0
+    tfirst = 0
+    tlast = 0
+    c = li[0]
+    t = li[0]
+    for i in range(1, len(li)):
+        num = li[i]
+        if num < num + c:
+            c = num + c
+            clast = i
+
+        else:
+            c = num
+            cfirst = i
+            clast = i
+
+        if c > t:
+            t = c
+            tfirst, tlast = cfirst, clast
+
+    return t, tfirst, tlast
+
+
 if __name__ == "__main__":
     li = [-2, -3, 4, -1, -2, 1, 5, -3]
     print kadane(li)
+    print kadane_with_indices(li)
